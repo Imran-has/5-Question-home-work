1d6bea05d9afe37ed38bb77717621fc3fd906331
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-06 09:38:39 -0700

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,17 @@
+function isprim(number, divisor = 2) {
+    if (number <= 2) {
+        return number === 2;
+    }
+    if (number % divisor === 0) {
+        return false;
+    }
+    if (divisor * divisor > number) {
+        return true;
+    }
+    return isprim(number, divisor + 1);
+}
+console.log(isprim(19));
+console.log(isprim(20));
+console.log(isprim(17));
+console.log(isprim(15));
+export {};
