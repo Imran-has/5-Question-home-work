c9a01a00bb603d91f86279539f4d5ae012684e52
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-05-06 09:39:27 -0700

    Add files via upload

---
+++ Q3.ts
@@ -0,0 +1,16 @@
+function firstNonRepeatedCharacter(string:any){
+    for(let i=0 ;i <string.length;i++){
+        let char=string.charAt(i);
+        if(string.indexOf(char)===i && string.indexOf(char,i+1)===-1){
+            return char;
+        }
+    }
+  return null;
+}
+console.log(firstNonRepeatedCharacter("hello"));
+console.log(firstNonRepeatedCharacter("iimmraan"));
+            
+           
+    
+
+    
